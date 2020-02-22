c2e233914d59dac25f36bd601e838f73e85fb59e
Hein Khant Zaw <heinkhantzaw1@gmail.com>
2020-02-22 13:48:58 +0630

    feat : update category page

---
+++ Blog/categories.php
@@ -0,0 +1,81 @@
+<?php require_once('include/header.php') ?>
+    <link rel="stylesheet" href="assets/bootstrap.min.css">
+    <title>Categories - Taste the Joy</title>
+    </head>
+
+    <body>
+<script src="assets/js/jquery.min.js"></script>
+<script src="assets/bootstrap/js/bootstrap.min.js"></script>
+
+
+<?php require_once('include/navbar.php') ?>
+<header class="masthead">
+    <div id="example-box-Category" class="example-box">
+        <h1 class="text-center"><strong>Categories</strong></h1><span
+            class="text-center subheading"><br>Choose your favourite Category!<br><br></span>
+        <div class="background-shapes"></div>
+    </div>
+</header>
+<section id="portfolio" class="bg-light">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6 col-md-4 portfolio-item">
+                <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
+                    <div class="portfolio-hover">
+                        <div class="portfolio-hover-content"><i class="fa fa-external-link fa-3x"></i></div>
+                    </div><img class="img-fluid" src="assets/img/portfolio/1-thumbnail.jpg"></a>
+                <div class="portfolio-caption">
+                    <h4>Threads</h4>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 portfolio-item">
+                <a class="portfolio-link" data-toggle="modal" href="#portfolioModal2">
+                    <div class="portfolio-hover">
+                        <div class="portfolio-hover-content"><i class="fa fa-external-link fa-3x"></i></div>
+                    </div><img class="img-fluid" src="assets/img/portfolio/2-thumbnail.jpg"></a>
+                <div class="portfolio-caption">
+                    <h4>Explore</h4>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 portfolio-item">
+                <a class="portfolio-link" data-toggle="modal" href="#portfolioModal3">
+                    <div class="portfolio-hover">
+                        <div class="portfolio-hover-content"><i class="fa fa-external-link fa-3x"></i></div>
+                    </div><img class="img-fluid" src="assets/img/portfolio/3-thumbnail.jpg"></a>
+                <div class="portfolio-caption">
+                    <h4>Finish</h4>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 portfolio-item">
+                <a class="portfolio-link" data-toggle="modal" href="#portfolioModal4">
+                    <div class="portfolio-hover">
+                        <div class="portfolio-hover-content"><i class="fa fa-external-link fa-3x"></i></div>
+                    </div><img class="img-fluid" src="assets/img/portfolio/4-thumbnail.jpg"></a>
+                <div class="portfolio-caption">
+                    <h4>Lines</h4>
+
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 portfolio-item">
+                <a class="portfolio-link" data-toggle="modal" href="#portfolioModal5">
+                    <div class="portfolio-hover">
+                        <div class="portfolio-hover-content"><i class="fa fa-external-link fa-3x"></i></div>
+                    </div><img class="img-fluid img-fluid" src="assets/img/portfolio/5-thumbnail.jpg"></a>
+                <div class="portfolio-caption">
+                    <h4>Southwest</h4>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-4 portfolio-item">
+                <a class="portfolio-link" data-toggle="modal" href="#portfolioModal6">
+                    <div class="portfolio-hover">
+                        <div class="portfolio-hover-content"><i class="fa fa-external-link fa-3x"></i></div>
+                    </div><img class="img-fluid" src="assets/img/portfolio/6-thumbnail.jpg"></a>
+                <div class="portfolio-caption">
+                    <h4>Window</h4>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<script src="assets/js/agency.js"></script>
+<?php require_once('include/footer.php'); ?>
