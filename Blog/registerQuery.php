1ff1212fd7ace8b8f464527b200442cfcf93ca7c
Thiri-Chan-Nyein <thirichannyein@uit.edu.mm>
2020-02-26 21:29:57 +0630

    update login

--- Blog/registerQuery.php
+++ Blog/registerQuery.php
@@ -4,6 +4,13 @@ $firstName = $_POST['firstName'];
 $lastName = $_POST['lastName'];
 $email = $_POST['email'];
 $password = $_POST['password'];
+$slquery = "SELECT 1 FROM register WHERE email = '$email'";
+$selectresult = mysqli_query($slquery);
+if(mysqli_num_rows($selectresult)>0)
+{
+    die('email already exists');
+}
+
 $sql = "INSERT INTO reg (firstName,lastName,email,password,createdDate,modifiedDate) VALUES ('" . $firstName . "','" . $lastName . "','" . $email . "','" . $password . "',now(),now())";
 mysqli_query($conn, $sql);
 
