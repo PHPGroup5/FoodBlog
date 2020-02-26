f2e0d30d53d16f37078195950c964a071c81bae4
Khant-haythi <khanthaythitun@uit.edu.mm>
2020-02-26 23:31:20 +0630

    updated index.php

--- Blog/index.php
+++ Blog/index.php
@@ -22,66 +22,26 @@ if ($_SESSION["loginStatus"] == 0) {
 <div class="container">
     <div class="block-heading"></div>
     <div class="block-content" style="padding: 80px;">
-        <div class="clean-blog-post" style="padding-bottom: 70px;">
+        
+        <?php
+            include("config.php");
+            $result = mysqli_query($conn, "SELECT * FROM post ORDER BY created_date DESC");
+
+            while ($row = mysqli_fetch_assoc($result)):?>
+        <div class="clean-blog-post" style="padding-bottom: 40px;">
             <div class="row">
-                <div class="col-lg-5"><img class="rounded img-fluid" src="assets/img/Sample.png"></div>
+                <div class="col-lg-5"><img class="img-fluid" 
+                    href="#" src="../Admin/covers/<?php echo $row['photo'] ?>"></div>
                 <div class="col-lg-7">
-                    <h3>Imperial Garden Seafood</h3>
-                    <div class="info"><span class="text-muted">Jan 16, 2018 by&nbsp;<a
-                                    href="blogpost1.php">John Smith</a></span></div>
-                    <p>Today We are going to introduce the review of Imperial Garden Rose Garden Hotel.It offers two
-                        types of Menu, Al La Carte Buffet and A La Carte Menu. Al La Carte Buffet is available for Lunch
-                        and Dinner. For adult 16,000ks+ and for child 10,000ks+ . The time limit is 2 hours.</p>
-                    <button class="btn btn-outline-primary btn-sm" type="button"><a href="blogpost1.php">Read More</a>
-                    </button>
-                </div>
-            </div>
-        </div>
-        <div class="clean-blog-post" style="padding-bottom: 70px;">
-            <div class="row">
-                <div class="col-lg-5"><img class="rounded img-fluid" src="assets/img/Astons.png"></div>
-                <div class="col-lg-7">
-                    <h3>Lorem Ipsum dolor sit amet</h3>
-                    <div class="info"><span class="text-muted">Jan 16, 2018 by&nbsp;<a href="#">John Smith</a></span>
+                    <h3><?php echo $row['title']?></h3>
+                    <div class="info"><span class="text-muted"><?php echo $row['created_date']?></span>
                     </div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in,
-                        mattis vitae leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna,
-                        dignissim nec auctor in, mattis vitae
-                        leo.</p>
+                    <p><?php echo $row['content']?></p>
                     <button class="btn btn-outline-primary btn-sm" type="button">Read More</button>
                 </div>
             </div>
         </div>
-        <div class="clean-blog-post" style="padding-bottom: 70px;">
-            <div class="row">
-                <div class="col-lg-5"><img class="rounded img-fluid" src="assets/img/Sample.png"></div>
-                <div class="col-lg-7">
-                    <h3>Lorem Ipsum dolor sit amet</h3>
-                    <div class="info"><span class="text-muted">Jan 16, 2018 by&nbsp;<a href="#">John Smith</a></span>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in,
-                        mattis vitae leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna,
-                        dignissim nec auctor in, mattis vitae
-                        leo.</p>
-                    <button class="btn btn-outline-primary btn-sm" type="button">Read More</button>
-                </div>
-            </div>
-        </div>
-        <div class="clean-blog-post">
-            <div class="row">
-                <div class="col-lg-5"><img class="rounded img-fluid" href="#" src="assets/img/Astons.png"></div>
-                <div class="col-lg-7">
-                    <h3>Lorem Ipsum dolor sit amet</h3>
-                    <div class="info"><span class="text-muted">Jan 16, 2018 by&nbsp;<a href="#">John Smith</a></span>
-                    </div>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna, dignissim nec auctor in,
-                        mattis vitae leo. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna,
-                        dignissim nec auctor in, mattis vitae
-                        leo.</p>
-                    <button class="btn btn-outline-primary btn-sm" type="button">Read More</button>
-                </div>
-            </div>
-        </div>
+    <?php endwhile; ?>
     </div>
 </div>
 <?php require_once('include/footer.php'); ?>
